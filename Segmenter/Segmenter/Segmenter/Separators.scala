2c8af8f2018001b88ddb1f00ff976d330aa2766b
JosefCech <cechjoe@gmail.com>
2013-01-06 21:15:02 +0100

    sep

--- Segmenter/Segmenter/Segmenter/Separators.scala
+++ Segmenter/Segmenter/Segmenter/Separators.scala
@@ -1,5 +1,13 @@
 package Segmenter
+import java.io._
 
 object Separators extends App {
 
+  def files : Array[File] =  { 
+    val f = new File("data")   
+    if (f.isDirectory)  f.listFiles
+    else Array[File]{f}   
+  }
+  
+ files. 
 }
